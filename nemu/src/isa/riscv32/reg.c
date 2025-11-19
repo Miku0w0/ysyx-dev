ea0fba843d345f1b76dd7a82fab1ae3dc54c4130
20238131022-Li Jinyuan <20238131022@m.scnu.edu.cn>
2025-11-19 14:36:31 +0800

    expr读取寄存器的值，实现了监视点triggered的功能

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,5 +31,40 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+    *success = true;
+  if (strcmp(s, "zero") == 0 || strcmp(s, "0") == 0) return 0;
+  if (strcmp(s, "ra") == 0) return cpu.gpr[1];
+  if (strcmp(s, "sp") == 0) return cpu.gpr[2];
+  if (strcmp(s, "gp") == 0) return cpu.gpr[3];
+  if (strcmp(s, "tp") == 0) return cpu.gpr[4];
+  if (strcmp(s, "t0") == 0) return cpu.gpr[5];
+  if (strcmp(s, "t1") == 0) return cpu.gpr[6];
+  if (strcmp(s, "t2") == 0) return cpu.gpr[7];
+  if (strcmp(s, "s0") == 0) return cpu.gpr[8];
+  if (strcmp(s, "s1") == 0) return cpu.gpr[9];
+  if (strcmp(s, "a0") == 0) return cpu.gpr[10];
+  if (strcmp(s, "a1") == 0) return cpu.gpr[11];
+  if (strcmp(s, "a2") == 0) return cpu.gpr[12];
+  if (strcmp(s, "a3") == 0) return cpu.gpr[13];
+  if (strcmp(s, "a4") == 0) return cpu.gpr[14];
+  if (strcmp(s, "a5") == 0) return cpu.gpr[15];
+  if (strcmp(s, "a6") == 0) return cpu.gpr[16];
+  if (strcmp(s, "a7") == 0) return cpu.gpr[17];
+  if (strcmp(s, "s2") == 0) return cpu.gpr[18];
+  if (strcmp(s, "s3") == 0) return cpu.gpr[19];
+  if (strcmp(s, "s4") == 0) return cpu.gpr[20];
+  if (strcmp(s, "s5") == 0) return cpu.gpr[21];
+  if (strcmp(s, "s6") == 0) return cpu.gpr[22];
+  if (strcmp(s, "s7") == 0) return cpu.gpr[23];
+  if (strcmp(s, "s8") == 0) return cpu.gpr[24];
+  if (strcmp(s, "s9") == 0) return cpu.gpr[25];
+  if (strcmp(s, "s10") == 0) return cpu.gpr[26];
+  if (strcmp(s, "s11") == 0) return cpu.gpr[27];
+  if (strcmp(s, "t3") == 0) return cpu.gpr[28];
+  if (strcmp(s, "t4") == 0) return cpu.gpr[29];
+  if (strcmp(s, "t5") == 0) return cpu.gpr[30];
+  if (strcmp(s, "t6") == 0) return cpu.gpr[31];
+
+  *success = false;
   return 0;
 }
