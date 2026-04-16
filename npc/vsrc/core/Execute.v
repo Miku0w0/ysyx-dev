ff2c6edf61182a24e0cf8d58f673dbf67d739984
Li Jinyuan <20238131022@m.scnu.edu.cn>
2026-04-16 14:08:55 +0800

    feat: 完成所有基础算术与逻辑运算指令的重构与验证

--- npc/vsrc/core/Execute.v
+++ npc/vsrc/core/Execute.v
@@ -20,7 +20,13 @@ module Execute (
             4'b0000: alu_out = rdata1 + src2; 
             4'b1000: alu_out = rdata1 - src2; 
             4'b0001: alu_out = rdata1 << src2[4:0]; 
-            
+            4'b0100: alu_out = rdata1 ^ src2;          
+            4'b0110: alu_out = rdata1 | src2;           
+            4'b0111: alu_out = rdata1 & src2;           
+            4'b0010: alu_out = ($signed(rdata1) < $signed(src2)) ? 32'd1 : 32'd0; 
+            4'b0011: alu_out = (rdata1 < src2) ? 32'd1 : 32'd0; 
+            4'b0101: alu_out = rdata1 >> src2[4:0];     
+            4'b1101: alu_out = $signed(rdata1) >>> src2[4:0]; 
             default: alu_out = rdata1 + src2;
         endcase
     end
