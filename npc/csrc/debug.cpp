67580ecaee044ffa40d2baf20938514e4956e9b2
Li Jinyuan <20238131022@m.scnu.edu.cn>
2026-05-08 22:32:22 +0800

    添加批量测试功能，cpu-tests通过大部分

--- npc/csrc/debug.cpp
+++ npc/csrc/debug.cpp
@@ -3,15 +3,18 @@
 #include <cstdlib>
 
 extern "C" void set_ebreak() {
-  printf("\033[1;32m[NPC] HIT EBREAK AT PC = 0x%08x\033[0m\n", dut->pc);
+  printf("\033[1;38;5;28m[NPC] HIT EBREAK AT PC = 0x%08x\033[0m\n", dut->pc);
 
   if (dut->a0 == 0) {
-    printf("\033[1;32m[NPC] HIT GOOD TRAP!\033[0m\n");
+    printf("\033[1;38;5;28m[NPC] HIT GOOD TRAP!\033[0m\n");
+    exit(0); // BAD TRAP -> 失败退出
   } else {
-    printf("\033[1;31m[NPC] HIT BAD TRAP! (code: 0x%08x)\033[0m\n", dut->a0);
+    printf("\033[1;38;5;196m[NPC] HIT BAD TRAP! (code: 0x%08x)\033[0m\n",
+           dut->a0);
+    exit(1); // BAD TRAP -> 失败退出
   }
 
   if (tfp)  tfp->close();
 
-  exit(0);
+
 }
--- npc/vsrc/core/top.v
+++ npc/vsrc/core/top.v
@@ -206,6 +206,7 @@ module top (
     // 仿真
     always @(posedge clk) begin
         if (!reset) begin
+            if ($test$plusargs("trace")) begin
             // ===== itrace =====
             $write("[%04d] PC:%08h  INST:%08h ", inst_cnt, pc, inst);
             // ===== 写回 =====
@@ -222,8 +223,10 @@ module top (
                 $write("MEM: -----------");
             $write("\n");
             // ===== ebreak =====
+            end
             if (inst == 32'h00100073) begin
                 $display("[EBREAK] hit at PC=%08h", pc);
+                $display("[DEBUG] Register a0 (x10) content: %d (hex: %08h)", a0, a0);
                 set_ebreak();
                 $finish;
             end
